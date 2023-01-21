6a13bfc3a74689b71526d2b314cf935407d7e2a7
ericginos <ericginos@gmail.com>
2023-01-20 17:54:48 -0800

    Updates

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-import * as THREE from '../node_modules/three/build/three.module.js';//'../three';
+//import * as THREE from '../node_modules/three/build/three.module.js';//'../three';
 // import { InteractionManager } from "../three.interactive";
 
 // import { OrbitControls } from '../three/examples/jsm/controls/OrbitControls';
